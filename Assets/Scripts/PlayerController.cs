2c093a11f4f39ff19b885ac375e044467dcd1b3d
meyu <yrrvknone@gmail.com>
2018-02-05 07:16:20 +0800

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public int count;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		countText.text = "";
+		winText.text = "";
+	}
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.R)) {
+			Application.LoadLevel(0);
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick"))
+		{
+			other.gameObject.SetActive (false);
+		}
+		MakeCount ();
+	}
+
+	void MakeCount () {
+		count += 1;
+		countText.text = count.ToString ();
+		if (count >= 12) {
+			winText.text = "You done...?";
+		}
+	}
+
+
+}
